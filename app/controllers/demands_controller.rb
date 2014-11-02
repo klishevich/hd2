cf56ef09ff8575a2150956319c460bddaf0b6ad4
klisha <mans1@ngs.ru>
2014-11-02 08:49:36 +0300

    autocomplete example

--- app/controllers/demands_controller.rb
+++ app/controllers/demands_controller.rb
@@ -3,6 +3,21 @@ class DemandsController < ApplicationController
     @demands=Demand.all
   end
 
+  def index  
+  # I will explain this part in a moment.
+  if params[:term]
+    @demands = Demand.where("description LIKE ?", "#{params[:term]}%")
+  else
+    @demands = Demand.all
+  end
+
+  respond_to do |format|  
+    format.html # index.html.erb  
+    # Here is where you can specify how to handle the request for "/people.json"
+    format.json { render :json => @demands.to_json }
+    end
+end
+
   def new
     @demand = Demand.new
   end
